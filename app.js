515c1a505b223c6579c70776b577f71bc795bcab
Rafael Muttoni <rmuttonic@gmail.com>
2020-05-04 17:14:01 -0300

    Navigating to Search page

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const App = async () => {
   const page = await browser.newPage();
   await page.goto('https://www.paodeacucar.com/');
 
+  // Go to Search page
+  const [ searchButton ] = await page.$x('//*[@id="light-header"]/div[5]/div[2]/div/div[2]/search/div/form/div/div/div/span/button');
+  await searchButton.click();
+  await page.waitFor(5000);
+
   // Testing with screenshot
   await page.screenshot({path: 'test.png'});
 
