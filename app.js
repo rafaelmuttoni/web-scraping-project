5f0184736934e8d6681cea7a50ac4db4df658141
Rafael Muttoni <rmuttonic@gmail.com>
2020-05-05 14:02:26 -0300

    Loop categories to execute app

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const XLSX = require('xlsx');
 
-const App = async () => {
+const Crawler = async (section) => {
   // Launch browser
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
@@ -20,7 +20,7 @@ const App = async () => {
 
     
   // Select category page and navigate to it
-  await page.goto('https://www.paodeacucar.com/secoes/4205/feira?qt=12&p=0&gt=list');
+  await page.goto(`https://www.paodeacucar.com/secoes/${section}/`);
   await page.waitFor(5000);
     
   // Scroll to bottom of the page
@@ -55,21 +55,26 @@ const App = async () => {
     return products;
   })
 
+  
+  let categoryName = productsData[0].category;
+  let categoryFileName = categoryName.split(' ').join('-');
+
+
   // Write excel file
   const excelData = XLSX.utils.json_to_sheet(productsData);
   const excelSheet = XLSX.utils.book_new();
-  XLSX.utils.book_append_sheet(excelSheet, excelData, 'PDA Data');
-  XLSX.writeFile(excelSheet, 'PDAData.xlsx');
+  XLSX.utils.book_append_sheet(excelSheet, excelData, categoryName);
+  XLSX.writeFile(excelSheet, `./data/${categoryFileName}.xlsx`);
 
   // Write txt file
   const productsString = JSON.stringify(productsData);
-  fs.writeFile("PDAData.txt", productsString, (err) => {
+  fs.writeFile(`./data/${categoryFileName}.txt`, productsString, (err) => {
     if (err) throw err;
     console.log('The file has been saved');
   })
 
   // Closing app
-  console.log('Done');
+  console.log(`Done with ${categoryName}`);
   await browser.close();
 };
 
@@ -86,6 +91,18 @@ async function scrollToBottom() {
       }
     }, delay);
   });
+}
+
+const sections = ['C7207', 'C4233', 'C4215', 'C4226', 'C4229', 'C4223', 'C4231', 'C4205', 'C4222', 'C4227'];
+
+async function App() {
+  const promises = sections.map(async (section) => {
+    await Crawler(section)
+  });
+
+  await Promise.all(promises);
+
+  console.log('Finished!')
 }
 
 App();
